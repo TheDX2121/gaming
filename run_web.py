9c3d265505d2f951c130ddc5a739a0ff6e24128b
TheDX2121 <129209401+TheDX2121@users.noreply.github.com>
2026-07-04 13:57:10 +0530

    Update run_web.py

--- run_web.py
+++ run_web.py
@@ -1,7 +1,16 @@
+import os
 from web_panel.app import create_app
 from shared.settings import Settings
 
 if __name__ == "__main__":
     settings = Settings.load()
     app = create_app()
-    app.run(host=settings.flask_host, port=settings.flask_port, debug=settings.flask_debug)
+
+    host = os.getenv("FLASK_HOST", "0.0.0.0")
+    port = int(os.getenv("PORT", os.getenv("FLASK_PORT", 5000)))
+
+    app.run(
+        host=host,
+        port=port,
+        debug=settings.flask_debug,
+    )
